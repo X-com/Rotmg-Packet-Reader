86ae460fa94c014c15b24351ff2b454b4c69d877
Xcom <sahandhesar@gmail.com>
2024-07-02 13:23:26 +0200

    Experimental code Image display change

--- src/test/java/experimental/DisplayIcons.java
+++ src/test/java/experimental/DisplayIcons.java
@@ -3,14 +3,55 @@ package experimental;
 import assets.IdToAsset;
 import assets.ImageBuffer;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeMap;
 
 public class DisplayIcons {
     static int bigId = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        readAllLines();
+    }
+
+    private static void readAllLines() throws IOException {
+//        java.io.InputStream is = DisplayIcons.class.getClassLoader().getResourceAsStream("assets/ObjectID.list");
+        File file = new File("assets/ObjectID.list");
+        String result = new java.io.BufferedReader(new java.io.InputStreamReader(Files.newInputStream(file.toPath()))).lines().collect(java.util.stream.Collectors.joining("\n"));
+        String[] split = result.split("\n");
+        HashSet<String> hash = new HashSet<>();
+        for(String line : split) {
+            String[] s = line.split(";");
+            if(!hash.contains(s[0])) {
+                int id = Integer.parseInt(s[0]);
+                extract(id);
+            }
+            hash.add(s[0]);
+        }
+    }
+
+    private static void extract(int id) throws IOException {
+        BufferedImage img = ImageBuffer.getImage(id);
+
+        try {
+            File f = new File("img/" + id + ".png");
+            ImageIO.write(img, "PNG", f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void display() {
         JFrame frame = new JFrame();
 
         frame.setSize(new Dimension(300, 300));
