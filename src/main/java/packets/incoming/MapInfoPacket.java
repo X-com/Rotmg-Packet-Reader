a7bbdea09b842218cdf01a073f230e060864dbf4
Xcom <sahandhesar@gmail.com>
2024-08-06 17:26:22 +0200

    Removed system printout

--- src/main/java/packets/incoming/MapInfoPacket.java
+++ src/main/java/packets/incoming/MapInfoPacket.java
@@ -121,7 +121,6 @@ public class MapInfoPacket extends Packet {
             maxRealmScore = buffer.readInt();
             currentRealmScore = buffer.readInt();
         }
-        System.out.println(this);
     }
 
     @Override
