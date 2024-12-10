c22982fd16cbd63be14740b9722f467f447bb1d4
CoreStar <92082512+Hardtek0657@users.noreply.github.com>
2024-12-09 23:59:46 -0500

    update Characterclass.java

--- src/main/java/tomato/realmshark/enums/CharacterClass.java
+++ src/main/java/tomato/realmshark/enums/CharacterClass.java
@@ -1,62 +1,32 @@
 package tomato.realmshark.enums;
 
-import java.util.Arrays;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
 
-/**
- * Character class enum to get class name and stats from class id.
- */
-public enum CharacterClass {
-    Rogue(768, 750, 252, 55, 25, 65, 75, 40, 50, new int[]{768, 800, 804}),
-    Archer(775, 750, 252, 75, 25, 55, 50, 40, 50, new int[]{775, 802, 796}),
-    Wizard(782, 700, 385, 75, 25, 50, 75, 40, 60, new int[]{782, 801, 803}),
-    Priest(784, 700, 385, 55, 25, 55, 60, 40, 75, new int[]{784, 805, 817}),
-    Warrior(797, 800, 252, 75, 25, 50, 50, 75, 50, new int[]{797, 798, 799}),
-    Knight(798, 800, 252, 50, 40, 50, 50, 75, 50, new int[]{797, 798, 799}),
-    Paladin(799, 800, 252, 55, 30, 55, 55, 60, 75, new int[]{797, 798, 799}),
-    Assassin(800, 750, 305, 65, 25, 65, 75, 40, 60, new int[]{768, 800, 804}),
-    Necromancer(801, 700, 385, 75, 25, 50, 60, 40, 75, new int[]{782, 801, 803}),
-    Huntress(802, 750, 305, 65, 25, 50, 60, 40, 50, new int[]{775, 802, 796}),
-    Trickster(804, 750, 252, 65, 25, 75, 75, 40, 60, new int[]{768, 800, 804}),
-    Mystic(803, 700, 385, 65, 25, 60, 65, 40, 75, new int[]{782, 801, 803}),
-    Sorcerer(805, 700, 385, 70, 25, 60, 60, 75, 60, new int[]{784, 805, 817}),
-    Ninja(806, 800, 252, 70, 25, 60, 70, 60, 70, new int[]{806, 785, 818}),
-    Samurai(785, 800, 252, 75, 30, 55, 55, 60, 60, new int[]{806, 785, 818}),
-    Bard(796, 750, 385, 55, 25, 55, 70, 45, 75, new int[]{775, 802, 796}),
-    Summoner(817, 700, 385, 60, 25, 60, 75, 40, 75, new int[]{784, 805, 817}),
-    Kensei(818, 800, 252, 65, 25, 60, 65, 60, 50, new int[]{806, 785, 818});
-    public static final CharacterClass[] CHAR_CLASS_LIST;
+import util.StringXML;
 
+public class CharacterClass {
+    private static final String PLAYERS_XML_PATH = "assets/xml/players.xml";
 
-    private final int life, mana, atk, def, spd, dex, vit, wis;
-    private final int id;
-    private final int[] weaponGroup;
-    private final int[] maxStats;
+    public final int id;
+    public final String name;
+    public final int life;
+    public final int mana;
+    public final int atk;
+    public final int def;
+    public final int spd;
+    public final int dex;
+    public final int vit; // Equivalent to HpRegen
+    public final int wis; // Equivalent to MpRegen
+    public int[] weaponGroup; // Mutable for updating after parsing
+    public final int[] maxStats;
 
-    private static final TreeMap<Integer, CharacterClass> CHARACTER_CLASS = new TreeMap<>();
-    private static final TreeMap<Integer, String> CLASS_NAME = new TreeMap<>();
-    private static final TreeMap<Integer, int[]> CLASS_MAX_STATS = new TreeMap<>();
-    private static final TreeMap<Integer, int[]> WEAPON_CLASSES = new TreeMap<>();
-    private static final TreeSet<Integer> CHARACTER_IDS = new TreeSet<>();
-
-    static {
-        CHAR_CLASS_LIST = CharacterClass.values().clone();
-        try {
-            for (CharacterClass o : CharacterClass.values()) {
-                CHARACTER_IDS.add(o.id);
-                CHARACTER_CLASS.put(o.id, o);
-                CLASS_NAME.put(o.id, o.toString());
-                WEAPON_CLASSES.put(o.id, o.weaponGroup);
-                CLASS_MAX_STATS.put(o.id, o.maxStats);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    CharacterClass(int id, int life, int mana, int atk, int def, int spd, int dex, int vit, int wis, int[] weaponGroup) {
+    CharacterClass(int id, String name, int life, int mana, int atk, int def, int spd, int dex, int vit, int wis, int[] weaponGroup) {
         this.id = id;
+        this.name = name;
         this.life = life;
         this.mana = mana;
         this.atk = atk;
@@ -67,6 +37,109 @@ public enum CharacterClass {
         this.wis = wis;
         this.weaponGroup = weaponGroup;
         maxStats = new int[]{life, mana, atk, def, spd, dex, vit, wis};
+    }
+
+    // Static structures to hold dynamic data
+    public static final CharacterClass[] CHAR_CLASS_LIST;
+    private static final TreeMap<Integer, CharacterClass> CHARACTER_CLASS = new TreeMap<>();
+    private static final TreeMap<Integer, String> CLASS_NAME = new TreeMap<>();
+    private static final TreeMap<Integer, int[]> CLASS_MAX_STATS = new TreeMap<>();
+    private static final TreeMap<Integer, int[]> WEAPON_CLASSES = new TreeMap<>();
+    private static final TreeSet<Integer> CHARACTER_IDS = new TreeSet<>();
+
+    // Static initialization block
+    static {
+        List<CharacterClass> charClassList = new ArrayList<>();
+        try {
+            FileInputStream file = new FileInputStream(PLAYERS_XML_PATH);
+            populateFromXML(new BufferedReader(new InputStreamReader(file)).lines().collect(Collectors.joining("\n")), charClassList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        CHAR_CLASS_LIST = charClassList.toArray(new CharacterClass[0]);
+        for (CharacterClass o : CHAR_CLASS_LIST) {
+            CHARACTER_IDS.add(o.id);
+            CHARACTER_CLASS.put(o.id, o);
+            CLASS_NAME.put(o.id, o.name);
+            WEAPON_CLASSES.put(o.id, o.weaponGroup);
+            CLASS_MAX_STATS.put(o.id, o.maxStats);
+        }
+    }
+
+    private static void populateFromXML(String rawXML, List<CharacterClass> charClassList) throws Exception {
+        // Parse the XML using StringXML
+        StringXML root = StringXML.getParsedXML(rawXML);
+
+        // Map to group character ids by Equipment's first integer
+        Map<Integer, List<Integer>> weaponGroups = new HashMap<>();
+        Map<Integer, Integer> weaponCharacter = new HashMap<>();
+
+        // Process each Object element
+        for (StringXML object : root) {
+            if (!object.name.equals("Object")) continue; // Skip non-Object nodes
+
+            // Extract id and name
+            String name = object.children.stream()
+                    .filter(child -> child.name.equals("id"))
+                    .map(child -> child.value)
+                    .findFirst()
+                    .orElse(null);
+
+            int id = object.children.stream()
+                    .filter(child -> child.name.equals("type"))
+                    .mapToInt(child -> Integer.decode(child.value))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("Missing type attribute"));
+
+            // Extract individual max stats
+            int life = getMaxValue(object, "MaxHitPoints");
+            int mana = getMaxValue(object, "MaxMagicPoints");
+            int atk = getMaxValue(object, "Attack");
+            int def = getMaxValue(object, "Defense");
+            int spd = getMaxValue(object, "Speed");
+            int dex = getMaxValue(object, "Dexterity");
+            int vit = getMaxValue(object, "HpRegen");
+            int wis = getMaxValue(object, "MpRegen");
+
+            // Extract Equipment and determine weapon group
+            String equipment = "";
+            for (StringXML child : object.children) {
+                if (child.name.equals("Equipment")) {
+                    for (StringXML stringXML : child.children) {
+                        equipment = stringXML.value;
+                        break;
+                    }
+                    break;
+                }
+            }
+            String[] equipmentItems = equipment.split(",");
+            int weaponId = Integer.decode(equipmentItems[0].trim());
+            weaponGroups.computeIfAbsent(weaponId, k -> new ArrayList<>()).add(id);
+            weaponCharacter.put(id, weaponId);
+
+            // Create CharacterClass instance
+            charClassList.add(new CharacterClass(id, name, life, mana, atk, def, spd, dex, vit, wis, new int[0])); // Weapon group will be set later
+        }
+
+        // Update weapon groups for each CharacterClass
+        for (CharacterClass character : charClassList) {
+            List<Integer> group = weaponGroups.get(weaponCharacter.get(character.id));
+            if (group != null) {
+                character.weaponGroup = group.stream().mapToInt(Integer::intValue).toArray();
+            }
+        }
+    }
+
+    private static int getMaxValue(StringXML object, String tagName) {
+        return object.children.stream()
+                .filter(child -> child.name.equals(tagName))
+                .map(child -> child.children.stream()
+                        .filter(attr -> attr.name.equals("max"))
+                        .mapToInt(attr -> Integer.parseInt(attr.value))
+                        .findFirst()
+                        .orElse(0))
+                .findFirst()
+                .orElse(0);
     }
 
     /**
